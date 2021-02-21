7e12c9533044b6db0c5defa77b537ff691a01677
kousikr26 <kousik18@iitg.ac.in>
2021-02-22 03:46:43 +0530

    extension

---
+++ background.js
@@ -0,0 +1,24 @@
+chrome.commands.onCommand.addListener(function (command) {
+    console.log('Command:', command);
+    if (command == "quick-reply-in"){
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { message: "reply-in" }, function (response) {
+                
+            });
+        });
+    }
+    else if (command == "quick-reply-out") {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { message: "reply-out" }, function (response) {
+                
+            });
+        });
+    }
+    else if (command =="send"){
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { message: "send" }, function (response) {
+                
+            });
+        });
+    }
+});
