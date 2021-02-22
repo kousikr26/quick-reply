93a0f040369cba9b7540c5449b643a8fdbc0dd6e
kousikr26 <kousik18@iitg.ac.in>
2021-02-22 14:48:08 +0530

    Changes

--- quick-reply-chrome/background.js
+++ quick-reply-chrome/background.js
@@ -1,13 +1,13 @@
 chrome.commands.onCommand.addListener(function (command) {
     console.log('Command:', command);
-    if (command == "quick-reply-in"){
+    if (command == "quick-reply-out"){
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             chrome.tabs.sendMessage(tabs[0].id, { message: "reply-in" }, function (response) {
                 
             });
         });
     }
-    else if (command == "quick-reply-out") {
+    else if (command == "quick-reply-in") {
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             chrome.tabs.sendMessage(tabs[0].id, { message: "reply-out" }, function (response) {
                 
